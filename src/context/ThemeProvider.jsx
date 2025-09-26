06939e322b5875b8a27399a56377dcc5a9c31b9a
bharatroy4D <bharatroy1005@gmail.com>
2025-09-26 13:15:34 +0600

    themeColor change, light to dark

--- src/context/ThemeProvider.jsx
+++ src/context/ThemeProvider.jsx
@@ -6,7 +6,7 @@ const ThemeContext = createContext();
 export const useTheme = () => useContext(ThemeContext);
 
 const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
 
   // toggle function
   const toggleTheme = () => {
